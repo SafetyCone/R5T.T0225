bd7bd68ea43aedb8443b98a74877f58060991172
davidcoats <david@getsafetycone.com>
2023-11-05 11:53:09 -0800

    Interim changes.

---
+++ source/R5T.T0225.T000/Code/_Types/_Classes/BaseType_001.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using R5T.T0142;
+
+
+namespace R5T.T0225.T000
+{
+    /// <summary>
+    /// An example base type for an example type hierarchy.
+    /// </summary>
+    [UtilityTypeMarker]
+    public abstract class BaseType_001
+    {
+    }
+}
---
+++ source/R5T.T0225.T000/Code/_Types/_Classes/DerivedType_002.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using R5T.T0142;
+
+
+namespace R5T.T0225.T000
+{
+    /// <summary>
+    /// An example derived type in an example type hierarchy.
+    /// </summary>
+    [UtilityTypeMarker]
+    public class DerivedType_002 : BaseType_001
+    {
+    }
+}
